4c27d0100b3a3a3ffcd69e93c1131ad8edb85baf
evanator05 <watsonevan2005@gmail.com>
2021-11-26 09:28:12 -0700

    gttgrgrg

--- main.py
+++ main.py
@@ -5,3 +5,15 @@ app = Flask(__name__)
 @app.route('/')
 def hello():
     return "hello world"
+
+def getUserIndex(data, username):
+    for i in range(len(data["users"])):
+        if data["users"][i]["name"]["username"] == username:
+            return i
+    else:
+        return None
+
+def addNewUser():
+    file = open("users.json", "w+")
+
+    
