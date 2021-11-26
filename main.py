fad8da97cde8c913957c94bde239bc1833b2e256
evanator05 <watsonevan2005@gmail.com>
2021-11-26 09:50:07 -0700

    gfdbgfbfbgbgf

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, render_template
+import os, json
 
 app = Flask(__name__)
 
@@ -13,7 +14,34 @@ def getUserIndex(data, username):
     else:
         return None
 
-def addNewUser():
-    file = open("users.json", "w+")
+def addNewUser(userData):
+    file = open("users.json", "r+")
 
-    
+    if os.stat("users.json").st_size == 0:
+        template = {
+            "users":[]
+        }
+        file.write(json.dumps(template))
+
+    userList = file.read()
+    userList = json.loads(userList)
+    userList["users"].append(userData)
+    file.truncate()
+    file.write(json.dumps(userList))
+    print(userList)
+    file.close()
+
+def makeUser(username, password, firstName, lastName, bio):
+    user = { #make user JSON object
+        "name": {
+            "username": username,
+            "firstName": firstName,
+            "lastName": lastName
+        },
+        "target":"Picked December First",
+        "password": password, #password encrypted with sha256 making it uncrackable
+        "bio": bio
+    }
+    return user
+
+addNewUser(makeUser("evanator", "5612732163", "evan", "watson", "my name is evan"))
