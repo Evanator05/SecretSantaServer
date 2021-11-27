cc1baf129f71031f97334786acdd0fb94bd3e180
Evanator05 <watsonevan2005@gmail.com>
2021-11-26 21:33:59 -0700

    76ytugtfygvhj

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from netcode import addUser
 
 app = Flask(__name__)
 @app.route('/', methods=['POST'])
+
 def result():
     addUser.addNewUser(request.form)
-    return 'Received !' # response to your request.
 
 if __name__ == "__main__":
     app.run(debug=True)
